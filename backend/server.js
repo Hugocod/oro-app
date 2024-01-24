dd142a9281f1f6c1782fcd4bbe3e5df1f5b521f8
hugocode90 <ugo@boolean.careers>
2024-01-24 13:48:24 +0100

    testing progress

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,28 @@ mongoose.connect('mongodb://localhost:27017/oro', {
 })
 
 // Definisci un modello Mongoose per i dati
-const DataModel = mongoose.model('Data', new mongoose.Schema({ value: String }))
+const dataSchema = new mongoose.Schema(
+    {
+        // ... i tuoi altri campi ...
+        oroPerLavori: Number,
+        oroCalo: Number,
+        acconti: Number,
+        costo: Number,
+        saldo: Number,
+        nomeCliente: String,
+    },
+    {
+        timestamps: true, // Abilita la creazione automatica dei campi createdAt e updatedAt
+    }
+)
+
+const DataModel = mongoose.model('Data', dataSchema)
 
 // Endpoint per ricevere i dati
 app.post('/submit-data', async (req, res) => {
     try {
-        const newData = new DataModel({ value: req.body.value })
+        console.log('test', req.body)
+        const newData = new DataModel(req.body)
         await newData.save()
         res.status(201).send('Dato salvato')
     } catch (error) {
@@ -44,6 +60,19 @@ app.delete('/delete-all', async (req, res) => {
         res.status(200).send('Tutti i documenti sono stati cancellati.')
     } catch (error) {
         res.status(500).send('Errore durante la cancellazione dei documenti')
+    }
+})
+
+app.delete('/delete-data/:id', async (req, res) => {
+    try {
+        const result = await DataModel.deleteOne({ _id: req.params.id })
+        if (result.deletedCount > 0) {
+            res.send(`Documento con ID ${req.params.id} eliminato.`)
+        } else {
+            res.status(404).send(`Documento con ID ${req.params.id} non trovato.`)
+        }
+    } catch (error) {
+        res.status(500).send("Errore durante l'eliminazione del documento")
     }
 })
 
