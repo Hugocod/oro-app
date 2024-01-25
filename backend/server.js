7bb1026775bcca6b4ee38202c541ad9565831377
hugocode90 <ugo@boolean.careers>
2024-01-25 15:02:07 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+// Middleware per analizzare i corpi delle richieste JSON
+app.use(express.json())
+
 // Connessione a MongoDB
 const mongoDBUri = process.env.MONGODB_URI // Assicurati di impostare questa variabile d'ambiente
 mongoose
@@ -10,32 +13,24 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then(() => {
-        console.log('Connesso a MongoDB')
-    })
-    .catch(err => {
-        console.error('Errore durante la connessione a MongoDB:', err)
-    })
+    .then(() => console.log('Connesso a MongoDB'))
+    .catch(err => console.error('Errore durante la connessione a MongoDB:', err))
 
-// Endpoint di base per testare se il server è in esecuzione
-app.get('/', (req, res) => {
-    res.send('Server funzionante!')
-})
-
-// Scegli una porta per il server
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Server in ascolto sulla porta ${PORT}`)
-})
-
+// Schema e modello Mongoose per gli utenti
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
     },
 })
 
-module.exports = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema)
 
+// Endpoint di base per testare se il server è in esecuzione
+app.get('/', (req, res) => {
+    res.send('Server funzionante!')
+})
+
+// Endpoint per creare un utente
 app.post('/submit-data', async (req, res) => {
     try {
         const newUser = new User({ email: req.body.email })
@@ -44,4 +39,10 @@ app.post('/submit-data', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
+})
+
+// Scegli una porta per il server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`)
 })
