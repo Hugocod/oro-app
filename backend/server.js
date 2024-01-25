f3dc1dc5889799e6118713dcfd2ee073fc69b593
hugocode90 <ugo@boolean.careers>
2024-01-25 12:50:54 +0100

    added mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// Connessione a MongoDB
+const mongoDBUri = process.env.MONGODB_URI // Assicurati di impostare questa variabile d'ambiente
+mongoose
+    .connect(mongoDBUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connesso a MongoDB')
+    })
+    .catch(err => {
+        console.error('Errore durante la connessione a MongoDB:', err)
+    })
 
 // Endpoint di base per testare se il server è in esecuzione
 app.get('/', (req, res) => {
