74d46a485b4c51b3dc876d1ba3e0078520b63b7c
hugocode90 <ugo@boolean.careers>
2024-01-26 09:47:52 +0100

    submit on form fix

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,10 +1,10 @@
 <template>
-    <form>
+    <form @submit.prevent="submitValue">
         <div>
             <label for="UserEmail" class="block text-xs font-medium text-gray-700"> Email </label>
             <input type="text" v-model="dataEmail" id="UserEmail" placeholder="john@rhcp.com" class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm" />
         </div>
-        <button @submit.prevent="submitValue">submit</button>
+        <button type="submit">submit</button>
     </form>
 </template>
 
@@ -26,6 +26,7 @@ export default {
                 const payload = { email: this.dataEmail } // Crea un oggetto con la proprietà email
                 const response = await axios.post('https://test-app-jcw6.onrender.com/submit-data', payload)
                 console.log('Risposta:', response.data)
+                console.log(this.dataEmail)
             } catch (error) {
                 console.error('Errore', error)
             }
