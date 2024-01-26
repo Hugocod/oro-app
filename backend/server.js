291a5fe455743e4bc38fc359e0e0392d5f836f3f
hugocode90 <ugo@boolean.careers>
2024-01-26 09:55:41 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
-
 // Middleware per analizzare i corpi delle richieste JSON
 app.use(express.json())
+
+// Configura CORS per consentire richieste dal tuo dominio frontend
+const corsOptions = {
+    origin: 'https://wondrous-creponne-f5a910.netlify.app', // Sostituisci con il tuo dominio frontend
+    optionsSuccessStatus: 200, // Per alcuni browser legacy che usano HTTP status code 200 invece di 204
+}
+app.use(cors(corsOptions))
 
 // Connessione a MongoDB
 const mongoDBUri = process.env.MONGODB_URI // Assicurati di impostare questa variabile d'ambiente
